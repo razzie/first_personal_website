048902126cdd4668eeb072936c3776b3802d9c96
Gábor Görzsöny <gabor@gorzsony.com>
2015-04-05 01:29:42 +0200

    Google Analytics

--- core/main.js
+++ core/main.js
@@ -1,3 +1,11 @@
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-61561301-1', 'auto');
+ga('send', 'pageview');
+
 $(document).ready(function()
 {
 	if ((navigator.appVersion.indexOf("MSIE 10") !== -1) ||
@@ -71,8 +79,10 @@ $(document).ready(function()
 					content.data('page', url.split('/', 1));
 					content.html(result);
 					content.animate({opacity: 1}, 'fast');
-					history.pushState({}, document.title, url);
 					initContent();
+					
+					history.pushState({}, document.title, url);
+					ga('send', 'pageview', url);
 				}
 			});
 		});
